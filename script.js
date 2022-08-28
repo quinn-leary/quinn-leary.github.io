e3fa793fbd416666dae2ee108342249b4c560566
quinn-leary <112227847+quinn-leary@users.noreply.github.com>
2022-08-28 02:14:51 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+   $( document ).ready(function() {  
+
+    if(window.location.href === "http://stackoverflow.com/questions/"){
+      $(".inner").width(1080);
+    }else{
+      $(".inner").width(620);
+    }
+}  
