21cf5ea51c00ad899aef22025636c62da6f7ccac
quinn-leary <112227847+quinn-leary@users.noreply.github.com>
2022-08-28 02:17:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
    $( document ).ready(function() {  
 
-    if(window.location.href === "http://stackoverflow.com/questions/"){
+    if(window.location.href === "https://quinn-leary.github.io/experience.html"){
       $(".inner").width(1080);
     }else{
       $(".inner").width(620);
